254d75c06997585dacb9c4beaa80b9dc4a0580f6
Alejandro Almagro <alejandroaf2005@gmail.com>
2026-05-24 12:54:39 +0200

    feat: Prueba

--- src/test/java/com/tareas/task_api/TaskApiApplicationTests.java
+++ src/test/java/com/tareas/task_api/TaskApiApplicationTests.java
@@ -1,13 +1,27 @@
 package com.tareas.task_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class TaskApiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
 	void contextLoads() {
 	}
+
 
 }
